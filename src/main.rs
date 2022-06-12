d4c5af4d484dc03edc3db201341bc82af86dd86d
Jan Ole Hüser <j0le@users.noreply.github.com>
2022-06-12 22:44:37 +0200

    Remove "use" directive

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
     for arg in args.iter() {
         println!("{}", arg);
     }
