1e7f32c493223b3048eacce7b4f33f7d6eb20c56
Jan Ole Hüser <j0le@users.noreply.github.com>
2022-06-13 00:25:29 +0200

    Use crate "clap"

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+
+    /// Should the program call itself
+    #[clap(long)]
+    callself: bool,
+}
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    for arg in args.iter() {
-        println!("{}", arg);
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
+
+    if args.callself {
+        println!("TODO: call self")
     }
 }
+
+
+//fn main() {
+//    let args: Vec<String> = std::env::args().collect();
+//    for arg in args.iter() {
+//        println!("{}", arg);
+//    }
+//}
+
 
 // Microsoft documentation
 // - https://docs.microsoft.com/en-us/cpp/c-language/parsing-c-command-line-arguments?view=msvc-170
