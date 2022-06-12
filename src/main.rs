c50521908a50bb64ed8df204fb09872d3c3e08df
Jan Ole Hüser <j0le@users.noreply.github.com>
2022-06-12 22:37:38 +0200

    Add links to microsoft documentation
    on how to split the commandline into arguments

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,6 @@ fn main() {
         println!("{}", arg);
     }
 }
+
+// https://docs.microsoft.com/en-us/archive/blogs/twistylittlepassagesallalike/everyone-quotes-command-line-arguments-the-wrong-way
+// https://docs.microsoft.com/en-us/cpp/c-language/parsing-c-command-line-arguments?view=msvc-170
