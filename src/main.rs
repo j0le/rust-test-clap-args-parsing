7074a769e107b02d9b7985c8930c6db5c6778959
Jan Ole Hüser <j0le@users.noreply.github.com>
2022-06-12 22:45:10 +0200

    Describe the hyper links in the comments

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,12 @@ fn main() {
     }
 }
 
-// https://docs.microsoft.com/en-us/archive/blogs/twistylittlepassagesallalike/everyone-quotes-command-line-arguments-the-wrong-way
-// https://docs.microsoft.com/en-us/cpp/c-language/parsing-c-command-line-arguments?view=msvc-170
+// Microsoft documentation
+// - https://docs.microsoft.com/en-us/cpp/c-language/parsing-c-command-line-arguments?view=msvc-170
+//
+// Microsoft Blog:
+// - https://docs.microsoft.com/en-us/archive/blogs/twistylittlepassagesallalike/everyone-quotes-command-line-arguments-the-wrong-way
+//
+// Rust standard library
+// - https://github.com/rust-lang/rust/issues/29494
+// - https://doc.rust-lang.org/std/os/windows/process/trait.CommandExt.html#tymethod.raw_arg
